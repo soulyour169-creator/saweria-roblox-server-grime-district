6da6056d9d9146960b3f31ba340254feece73978
soulyour169-creator <soulyour169@gmail.com>
2026-04-25 14:08:23 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@
 const { kv } = require("@vercel/kv");
 
 // Discord Webhook URL
-const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1491659680198692935/ckjpKMqdq8UHqIIBl9p8XjlmfWrVpOei4I9gntHecjV7gfNN4T24CvsdqZFy43tLe20U";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1497493880533422182/hnKgz_K00FTEM8sh-M7Jofq7Dk0L2qQRmziVhWtc8-PmCbepWXzJZsFeEcrsR9a6RxOO";
 
 async function redisCommand(args) {
   const url   = process.env.STORAGE_URL   || process.env.KV_REST_API_URL;
